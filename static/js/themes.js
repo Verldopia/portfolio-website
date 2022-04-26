ebb06031af40ead6117f951b6e6ff85e3bf02cdf
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-04-26 19:30:50 +0200

    22.17.2.1 Added themes, added JSON-files

--- static/js/themes.js
+++ static/js/themes.js
@@ -1,54 +1,66 @@
-export const themes = [
-  {
-    name: 'Beach',
-    slug: 'beach',
-    items: [
-      { name: 'primary-top', var: '#cc38b8' },
-      { name: 'primary-bottom', var: '#bd8c22' },
-      { name: 'primary-text', var: '#FFFFFF' },
-      { name: 'primary-border', var: '#181818' },
-      { name: 'primary-buttons', var: '#181818' },
-      { name: 'primary-nav__bg', var: 'rgba(255, 255, 255, 0.2)' },
-      { name: 'switch-img', var: 'url(../images/beach.png)' },
-    ],
-  },
-  {
-    name: 'Toxic',
-    slug: 'toxic',
-    items: [
-      { name: 'primary-top', var: '#194e1c' },
-      { name: 'primary-bottom', var: '#fbff01' },
-      { name: 'primary-text', var: '#ffff00' },
-      { name: 'primary-border', var: '#093211' },
-      { name: 'primary-buttons', var: '#093211' },
-      { name: 'primary-nav__bg', var: 'rgba(0, 0, 0, 0.4)' },
-      { name: 'switch-img', var: 'url(../images/toxic.png)' },
-    ],
-  },
-  {
-    name: 'Unicorn',
-    slug: 'unicorn',
-    items: [
-      { name: 'primary-top', var: '#e65da1' },
-      { name: 'primary-bottom', var: '#22b8bd' },
-      { name: 'primary-text', var: '#FFFFFF' },
-      { name: 'primary-border', var: '#8b267e' },
-      { name: 'primary-buttons', var: '#8b267e' },
-      { name: 'primary-nav__bg', var: 'rgba(255, 255, 255, 0.2)' },
-      { name: 'switch-img', var: 'url(../images/unicorn.png)' },
-    ],
-  },
-  {
-    name: 'Midnight',
-    slug: 'midnight',
-    items: [
-      { name: 'primary-top', var: '#00093a' },
-      { name: 'primary-bottom', var: '#aa61ee' },
-      { name: 'primary-text', var: '#FFFFFF' },
-      { name: 'primary-border', var: '#000000' },
-      { name: 'primary-buttons', var: '#000000' },
-      { name: 'primary-nav__bg', var: 'rgba(0, 0, 0, 0.2)' },
-      { name: 'switch-img', var: 'url(../images/midnight.png)' },
-    ],
-  },
-];
+import themes from '../data/themes.json' assert {type: 'json'};
+
+(() => {
+  const app = {
+    init() {
+      // Get defined themes from themes module
+      this.themes = themes;
+      this.root = document.documentElement;
+      this.index = localStorage.getItem('activeIndex');
+      this.index = this.index ?? 1;
+
+      this.cacheElements();
+      this.registerListeners();
+      this.shake();
+      this.changeDOMTheme();
+    },
+    cacheElements() {
+      this.$switch = document.querySelector('.switch__img');
+      this.$switchBox = document.querySelector('.switch-box');
+      this.$overlay = document.querySelector('.overlay__box');
+    },
+    registerListeners() {
+      this.$switch.addEventListener('click', () => {
+        // Loop through themes
+        this.activeThemeName =
+          this.themes[
+            this.index < this.themes.length ? this.index++ : (this.index = 0)
+          ];
+        this.index = this.index || 1;
+
+        // Set active state and delete after 1.5seconds
+        this.$switch.classList.add('active');
+        this.$overlay.classList.add('overlay');
+        setTimeout(() => {
+          this.$switch.classList.remove('active');
+        }, 1500);
+        setTimeout(() => {
+          this.$overlay.classList.remove('overlay');
+        }, 600);
+
+        // Change in local storage
+        localStorage.setItem('activeIndex', this.index);
+        localStorage.setItem('activeThemeName', this.activeThemeName.slug);
+        this.changeDOMTheme();
+      });
+    },
+    shake() {
+      setInterval(() => {
+        this.$switchBox.classList.add('shaking');
+      }, 3000);
+      setInterval(() => {
+        this.$switchBox.classList.remove('shaking');
+      }, 6000);
+    },
+    changeDOMTheme() {
+      const activeTheme = this.themes.find(
+        (theme) => theme.slug === localStorage.getItem('activeThemeName')
+      );
+
+      activeTheme.items.forEach((item) => {
+        this.root.style.setProperty(`--${item.name}`, item.var);
+      });
+    },
+  };
+  app.init();
+})();
