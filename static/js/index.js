c9117c881d65abde0527706687d810973a835084
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-04-26 20:54:54 +0200

    22.17.2.2 Completed JSON-rendering in index

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,50 @@
+import projects from '../data/projects.json' assert {type: 'json'};
+
 (() => {
     const app = {
         init() {
+            this.projects = projects;
             this.cacheElements();
             this.generateUI();
         },
         cacheElements() {
             this.$hamburger = document.querySelector('.hamburger-menu');
+            this.$tiles = document.querySelector('.container');
         },
         generateUI() {
             this.registerListeners();
+            this.$tiles.innerHTML += this.generateTile(this.projects);
         },
         registerListeners() {
-            this.$hamburger.addEventListener('click', this.listener)
+            this.$hamburger.addEventListener('click', this.toggler)
         },
-        listener() {
+        toggler() {
             this.$hamburgerItems = document.querySelector('.nav__list--hamburger');
             this.$hamburgerItems.classList.toggle('hamburger-selected');
+        },
+        generateTile() {
+            return this.projects.map((p) => {
+                if (p.workItem) {
+                    return `
+                    <div class="content-box ${p.important ? (p.breaking && p.important ? "box__large box__breaking" : "box__large") : ''} work-item" style="background-image: url(static/assets/images/${p.cover})">
+                    <div class="work-item__ribbon">
+                        <p class="ribbon__text">${p.title}</p>
+                    </div>
+                    <div class="work-item__bg">
+                        <div class="work-item__btns">
+                            <a class="link" href="${p.link.github}" target="_blank">
+                                <div class="link__icon github"></div>
+                                <span class="link__href">GitHub</span>
+                            </a>
+                            <a class="link" href="${p.link.details}" target="_blank">
+                                <div class="link__icon linkedin"></div>
+                                <span class="link__href">Details</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>`
+            }}).join('');
         }
-        }
+    }
     app.init()
 })();
