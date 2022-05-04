fe53031b023d6ea296b2c27b482365aadd3bed5a
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-05-04 22:22:29 +0200

    22.18.3.1 Added modal for projects

--- static/js/index.js
+++ static/js/index.js
@@ -8,19 +8,27 @@ import textItems from '../data/textItems.json' assert {type: 'json'};
             this.generateUI();
         },
         cacheElements() {
+            this.projects = projects;
+            this.textItems = textItems;
             this.$hamburger = document.querySelector('.hamburger-menu');
             this.$tiles = document.querySelector('.container');
+            this.$workItem = document.querySelectorAll('.work-item');
+            this.$mainContainer = document.querySelector('.main-container')
+            this.$modal = document.querySelector('.modal')
         },
         generateUI() {
+            this.generateTile();
             this.registerListeners();
-            this.generateTile(projects, textItems);
         },
         registerListeners() {
-            this.$hamburger.addEventListener('click', this.toggler)
-        },
-        toggler() {
-            this.$hamburgerItems = document.querySelector('.nav__list--hamburger');
             this.$html = document.querySelector('html');
+            this.$hamburger.addEventListener('click', this.hamburgerToggler);
+            this.$workItem.forEach(workItem => {
+                workItem.addEventListener('click', () => this.generateModal(workItem));
+            });
+        },
+        hamburgerToggler() {
+            this.$hamburgerItems = document.querySelector('.nav__list--hamburger');
             const lock = this.$hamburgerItems.classList.toggle('hamburger-selected');
             if (lock) {
                 this.$html.style.overflow="hidden";
@@ -28,10 +36,10 @@ import textItems from '../data/textItems.json' assert {type: 'json'};
                 this.$html.style.overflow="unset";
             }
         },
-        generateTile(projects, textItems) {
-            projects.push(...textItems);
-            for (let i = 0; i < projects.length; i++) {
-            const project = projects.find(e => e.order - 1 === i)
+        generateTile() {
+            this.projects.push(...this.textItems);
+            for (let i = 0; i < this.projects.length; i++) {
+            const project = this.projects.find(e => e.order - 1 === i)
             if(project.workItem) {
                 const items = `
                 <div class="content-box ${
@@ -39,8 +47,7 @@ import textItems from '../data/textItems.json' assert {type: 'json'};
                     (project.breaking && project.important ? 
                         "box__large box__breaking" 
                         : "box__large") 
-                        : ''} 
-                    work-item" style="background-image: url(static/assets/images/${project.images.cover})">
+                        : ''} work-item" data-id=${project.order} style="background-image: url(static/assets/images/${project.images.cover})">
                     <div class="work-item__ribbon">
                         <p class="ribbon__text">${project.title}</p>
                     </div>
@@ -65,22 +72,50 @@ import textItems from '../data/textItems.json' assert {type: 'json'};
                     <h3 class="margin-bottom">${project.title}</h3>
                     ${project.text ? `
                         <ul>
-                            <li>
-                                <p class="yellow">${
-                                    orangeTitle.title ? `<a class="yellow" href=${orangeTitle.url} target="_blank">${orangeTitle.title}</a>` 
-                                    : orangeTitle}</p>
-                                ${project.text.map((text) => 
-                                    `<li>${text.title ? `<a href=${text.url} target="_blank">${text.title}</a>` 
-                                    : `<p>${text}</p>`}</li>`).join('')}
-                            </li>
+                        <li>
+                            <p class="yellow">${
+                                orangeTitle.title ? `<a class="yellow" href=${orangeTitle.url} target="_blank">${orangeTitle.title}</a>` 
+                                : orangeTitle}</p>
+                            ${project.text.map((text) => 
+                                `<li>${text.title ? `<a href=${text.url} target="_blank">${text.title}</a>` 
+                                : `<p>${text}</p>`}</li>`).join('')}
+                        </li>
                         </ul>
                         ` : ""
                     }
                     ${project.button ? `<a href="./static/pages/contact.html" class="btn btn--yellow margin-top">Contact</a>` : ''}
                 </div>`
                 this.$tiles.innerHTML += items;
-            }};
-        }
+                this.cacheElements();
+            }}
+        },
+        generateModal (workItem) {
+            const project = this.projects.find(project => project.order == workItem.dataset.id);
+            console.log(project);
+            this.$modal.innerHTML = `
+            <div class="modal__box">
+                <button class="modal__close"></button>
+                <h2 class="modal__title">${project.title}</h2>
+                <h3 class="modal__year">This was programmed in ${project.year}.</h3>
+                <p class="modal__paragraph">${project.description}</p>
+                <div class="modal__image-box" style="background-image: url(static/assets/images/${project.images.cover})"></div>
+                <div class="work-item__bg">
+                    <div class="work-item__btns work-item__btns--modal">
+                        <a class="link" href="${project.link.github}" target="_blank">
+                            <div class="link__icon github"></div>
+                            <span class="link__href">GitHub</span>
+                        </a>
+                        <a class="link" href="${project.link.details}" target="_blank">
+                            <div class="link__icon linkedin"></div>
+                            <span class="link__href">Deployed</span>
+                        </a>
+                    </div>
+                </div>                
+            </div>
+            `
+            this.$close = document.querySelector('.modal__close');
+            this.$close.addEventListener('click', () => this.$modal.innerHTML = "");
+        },
     }
     app.init()
 })();
